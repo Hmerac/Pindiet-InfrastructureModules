c2a4c97f1bb1ef2b10a1aed2185e83a73a107be6
mertacikportali <mert.acikportali@siemens.com>
2018-10-05 12:36:51 +0300

    ID Fix

--- platform/vpc/rt.tf
+++ platform/vpc/rt.tf
@@ -9,7 +9,7 @@ resource "aws_route_table" "public_route_table" {
 
 # Create public route for Internet GW
 resource "aws_route" "public_internet_gateway" {
-  route_table_id         = "${aws_route_table.public_route_table}"
+  route_table_id         = "${aws_route_table.public_route_table.id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.internet_gateway.id}"
 
