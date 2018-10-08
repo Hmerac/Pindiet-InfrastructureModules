94aa74513103e3948f2e7de5a0806b7e18d0266a
mert.acikportali <mert.acikportali@siemens.com>
2018-10-08 11:59:09 +0300

    ALB is initialized v8

--- platform/alb/variables.tf
+++ platform/alb/variables.tf
@@ -3,6 +3,5 @@ variable "environment" {}
 variable "ext_alb_prefix" {}
 variable "ext_target_group_name" {}
 variable "region" {}
-variable "vpc_id" {}
 variable "vpc_state_bucket" {}
 variable "vpc_state_key" {}
