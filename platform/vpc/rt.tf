7eca7b33028af066621b0e04b9771ce821aec95f
mert.acikportali <mert.acikportali@siemens.com>
2018-10-08 13:48:29 +0300

    VPC is updated

--- platform/vpc/rt.tf
+++ platform/vpc/rt.tf
@@ -1,3 +1,7 @@
+##################################################
+########            Public RT             ########
+##################################################
+
 # Create public route table
 resource "aws_route_table" "public_route_table" {
   vpc_id = "${aws_vpc.main_vpc.id}"
@@ -18,6 +22,21 @@ resource "aws_route" "public_internet_gateway" {
   }
 }
 
+# Create associations for public subnets and public route tables
+resource "aws_route_table_association" "public_1_route_table_association" {
+  subnet_id      = "${aws_subnet.public_subnet_1.id}"
+  route_table_id = "${aws_route_table.public_route_table}"
+}
+
+resource "aws_route_table_association" "public_2_route_table_association" {
+  subnet_id      = "${aws_subnet.public_subnet_2.id}"
+  route_table_id = "${aws_route_table.public_route_table}"
+}
+
+##################################################
+########           Private RT             ########
+##################################################
+
 # Create private route table
 resource "aws_route_table" "private_route_table" {
   vpc_id = "${aws_vpc.main_vpc.id}"
@@ -36,4 +55,15 @@ resource "aws_route" "private_nat_gateway" {
   timeouts {
     create = "5m"
   }
+}
+
+# Create associations for private subnets and private route tables
+resource "aws_route_table_association" "private_1_route_table_association" {
+  subnet_id      = "${aws_subnet.private_subnet_1.id}"
+  route_table_id = "${aws_route_table.private_route_table}"
+}
+
+resource "aws_route_table_association" "private_2_route_table_association" {
+  subnet_id      = "${aws_subnet.private_subnet_2.id}"
+  route_table_id = "${aws_route_table.private_route_table}"
 }
