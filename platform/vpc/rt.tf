d03308f9e85d9f6e596276908fb97939b10360b7
mert.acikportali <mert.acikportali@siemens.com>
2018-10-09 13:24:01 +0300

    Route Table id fix

--- platform/vpc/rt.tf
+++ platform/vpc/rt.tf
@@ -23,7 +23,7 @@ resource "aws_route" "public_internet_gateway" {
 resource "aws_route_table_association" "public" {
   count          = "${length(var.public_subnets_cidr)}"
   subnet_id      = "${element(aws_subnet.public_subnet.*.id, count.index)}"
-  route_table_id = "${aws_route_table.public_route_table}"
+  route_table_id = "${aws_route_table.public_route_table.id}"
 }
 
 ##################################################
