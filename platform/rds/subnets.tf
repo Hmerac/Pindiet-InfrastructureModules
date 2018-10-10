c991d770aa9cf59fcc6b0b1ed7f68baa302cfe69
mert.acikportali <mert.acikportali@siemens.com>
2018-10-10 11:56:08 +0300

    Test v12

--- platform/rds/subnets.tf
+++ platform/rds/subnets.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "rds_subnet_group" {
-  name        = "${var.environment}-RDS-Subnet-Group"
+  name        = "${var.environment}-rds-subnet-group"
   description = "RDS Subnet Group"
   subnet_ids  = ["${data.terraform_remote_state.vpc_state.private_subnet_id_list}"]
 
