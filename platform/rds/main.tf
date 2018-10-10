8380f4354dcad2f4f7c5653dcf77e471169a7af4
mert.acikportali <mert.acikportali@siemens.com>
2018-10-10 12:00:08 +0300

    Test v13

--- platform/rds/main.tf
+++ platform/rds/main.tf
@@ -15,7 +15,6 @@ resource "aws_db_instance" "rds" {
   db_subnet_group_name   = "${aws_db_subnet_group.rds_subnet_group.id}"
   vpc_security_group_ids = ["${aws_security_group.rds_sg.id}"]
   skip_final_snapshot    = true
-  snapshot_identifier    = "${var.database_name}-${var.environment}-Snapshot"
   tags {
     Name        = "${var.database_name}"
     Environment = "${var.environment}"
