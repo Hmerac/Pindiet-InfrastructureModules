61adc60e9e5019ceb96c2cc4c3617d00562b1b3f
mert.acikportali <mert.acikportali@siemens.com>
2018-10-11 08:51:32 +0300

    Let there be two NATs

--- platform/vpc/eip.tf
+++ platform/vpc/eip.tf
@@ -1,10 +1,21 @@
-# Create Elastic IP for NAT GW
-resource "aws_eip" "ng_elastic_ip" {
+# Create Elastic IP for NAT GW 1
+resource "aws_eip" "ng_elastic_ip_1" {
   vpc      = true
   depends_on = ["aws_internet_gateway.internet_gateway"]
 
   tags {
-    Name = "${var.environment}-EIP"
+    Name = "${var.environment}-NG-EIP-1"
+    Environment = "${var.environment}"
+  }
+}
+
+# Create Elastic IP for NAT GW 2
+resource "aws_eip" "ng_elastic_ip_2" {
+  vpc      = true
+  depends_on = ["aws_internet_gateway.internet_gateway"]
+
+  tags {
+    Name = "${var.environment}-NG-EIP-2"
     Environment = "${var.environment}"
   }
 }
--- platform/vpc/gateways.tf
+++ platform/vpc/gateways.tf
@@ -8,14 +8,26 @@ resource "aws_internet_gateway" "internet_gateway" {
   }
 }
 
-# Create NAT GW
+# Create NAT GW for AZ-a
 resource "aws_nat_gateway" "nat_gateway" {
-  allocation_id = "${aws_eip.ng_elastic_ip.id}"
+  allocation_id = "${aws_eip.ng_elastic_ip_1.id}"
   subnet_id     = "${element(aws_subnet.public_subnet.*.id, 0)}"
-  depends_on    = ["aws_subnet.public_subnet", "aws_internet_gateway.internet_gateway"]
+  depends_on    = ["aws_subnet.public_subnet", "aws_eip.ng_elastic_ip_1", "aws_internet_gateway.internet_gateway"]
 
   tags {
-    Name        = "${var.environment}-${element(var.availability_zones, count.index)}-NGW"
+    Name        = "${var.environment}-${element(var.availability_zones, 0)}-NGW"
+    Environment = "${var.environment}"
+  }
+}
+
+# Create NAT GW for AZ-b
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = "${aws_eip.ng_elastic_ip_2.id}"
+  subnet_id     = "${element(aws_subnet.public_subnet.*.id, 1)}"
+  depends_on    = ["aws_subnet.public_subnet", "aws_eip.ng_elastic_ip_2", "aws_internet_gateway.internet_gateway"]
+
+  tags {
+    Name        = "${var.environment}-${element(var.availability_zones, 1)}-NGW"
     Environment = "${var.environment}"
   }
 }
