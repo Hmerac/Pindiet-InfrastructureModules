6e8ee225180a35a9df2dccc483842636debe354c
mert.acikportali <mert.acikportali@siemens.com>
2018-10-12 09:50:54 +0300

    Finalizing ASG v4 and Bastion update

--- platform/bastion/main.tf
+++ platform/bastion/main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "bastion_for_rds" {
   ami                         = "ami-0233214e13e500f77"
   instance_type               = "t2.micro"
   user_data                   = "${data.template_file.user_data.rendered}"
-  subnet_id                   = "${data.terraform_remote_state.vpc_state.private_subnet_id_list[0]}"
+  subnet_id                   = "${data.terraform_remote_state.vpc_state.public_subnet_id_list[0]}"
   vpc_security_group_ids      = ["${aws_security_group.bastion_security_group.id}"]
   key_name                    = "${var.bastion_keypair_name}"
   iam_instance_profile        = "${aws_iam_instance_profile.bastion_profile.id}"
