ab908c8b6d86bff1c8f794d4f1b89acda72bd98e
mert.acikportali <mert.acikportali@siemens.com>
2018-10-12 09:49:41 +0300

    Finalizing ASG v3

--- platform/asg/scripts/user-data.sh
+++ platform/asg/scripts/user-data.sh
@@ -3,6 +3,15 @@
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 sudo start amazon-ssm-agent
 
-mkdir /etc/ecs
-echo ECS_CLUSTER=${var.cluster_name} > /etc/ecs/ecs.config
-echo ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION=30m >> /etc/ecs/ecs.config
+# Write ECS config file
+mkdir -p /var/log/ecs /var/lib/ecs/data /etc/ecs && touch /etc/ecs/ecs.config
+cat << EOF > /etc/ecs/ecs.config
+ECS_DATADIR=/data
+ECS_ENABLE_TASK_IAM_ROLE=true
+ECS_ENABLE_TASK_IAM_ROLE_NETWORK_HOST=true
+ECS_LOGFILE=/log/ecs-agent.log
+ECS_AVAILABLE_LOGGING_DRIVERS=["json-file","awslogs"]
+ECS_LOGLEVEL=info
+ECS_CLUSTER=${var.cluster_name}
+ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION=30m
+EOF
