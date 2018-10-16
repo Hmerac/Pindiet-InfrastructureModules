24a5352e91b6d51bf9d6aed940d053ee41176fcf
mert.acikportali <mert.acikportali@siemens.com>
2018-10-16 15:20:52 +0300

    Lambda filename too long fix v2

--- platform/asg/lambda.tf
+++ platform/asg/lambda.tf
@@ -25,10 +25,10 @@ resource "aws_lambda_function" "lambda_function" {
 # Appropriate permissions for related Lambda script
 resource "aws_lambda_permission" "allow_cloudwatch" {
   statement_id    = "${aws_cloudwatch_event_rule.event_rule.name}"
-  action = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.lambda_function.function_name}"
-  principal = "events.amazonaws.com"
-  source_arn = "${aws_cloudwatch_event_rule.event_rule.arn}"
+  action          = "lambda:InvokeFunction"
+  function_name   = "${aws_lambda_function.lambda_function.function_name}"
+  principal       = "events.amazonaws.com"
+  source_arn      = "${aws_cloudwatch_event_rule.event_rule.arn}"
 }
 
 ##################################################
